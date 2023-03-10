a1ca49f894335292418b1334b6e1cf920287dce4
Shiv <190345305073@paruluniversity.ac.in>
2023-03-11 00:13:03 +0530

    Answers, auth, Questions

--- server/controllers/Questions.js
+++ server/controllers/Questions.js
@@ -3,15 +3,16 @@ import mongoose from 'mongoose'
 
 export const AskQuestion = async (req, res) => {
     const postQuestionData = req.body;
-    const postQuestion = new Questions({...postQuestionData, userId: req.userId});
+    const userId = req.userId;
+    const postQuestion = new Questions({...postQuestionData, userId});
     try {
         await postQuestion.save();
-        res.status(200).json("Posted a question successfully")
+        res.status(200).json("Posted a question successfully");
     } catch (error) {
         console.log(error)
-        res.status(409).json("Couldn't post a new question")
+        res.status(409).json("Couldn't post a new question");
     }
-}
+};
 
 export const getAllQuestions = async (req, res) => {
     try {
@@ -19,5 +20,20 @@ export const getAllQuestions = async (req, res) => {
         res.status(200).json(questionList);
     } catch (error) {
         res.status(404).json({ message: error.message });
+    }
+}
+
+export const deleteQuestion = async (req, res) =>{
+    // const { id:_id } = req.params;
+
+    // if (!mongoose.Types.ObjectId.isValid(_id)) {
+    //     return res.status(404).send("question unavailable...");
+    // }
+
+    try {
+        await Questions.findByIdAndRemove( _id );
+        res.status(200).json({ message: "successfully deleted..."})    
+    } catch (error) {
+        res.status(404).json({ message: error.message }) 
     }
 }
